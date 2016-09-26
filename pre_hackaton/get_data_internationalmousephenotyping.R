f5fabcde8987a3dac082c3468b470e7d0f40716b
Giovanni Dall'Olio <giovanni.m.dallolio@gsk.com>
2016-09-26 15:24:46 -0400

    PRE: converting mousephenotype to df

--- pre_hackaton/get_data_internationalmousephenotyping.R
+++ pre_hackaton/get_data_internationalmousephenotyping.R
@@ -9,3 +9,9 @@ library(tidyverse)
 #samples = c("MGI:1929293","MGI:1352447")
 #baseurl = "http://www.mousephenotype.org/phenoview/rest/measurements/download?mgiid=MGI:5760356,MGI:1929293,MGI:104874,MGI:1929296,MGI:1352447&parameter=IMPC_BWT_001_001"
 baseurl = "http://www.mousephenotype.org/phenoview/rest/measurements/download?mgiid=MGI:1914805,MGI:2142632,MGI:2384774,MGI:1914824,MGI:1914731,MGI:2146159,MGI:1916193,MGI:109583,MGI:1929293,MGI:1352447,MGI:95684,MGI:1924086,MGI:98715,MGI:96912,MGI:96912,MGI:3646952,MGI:3710397,MGI:3619332,MGI:88476,MGI:2443584,MGI:3039607&parameter=IMPC_BWT_001_001"
+baseurl = "datasets/mousephenotype/mousephenotypedata.json"
+
+
+resp = fromJSON(baseurl)
+resp.df = bind_rows(resp$measurements$measurements) %>% tbl_df
+
